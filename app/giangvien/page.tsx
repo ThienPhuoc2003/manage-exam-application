b59e52371c7832a452a93e64e3dcd217c97082fc
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2024-06-13 19:23:39 +0700

    Update page.tsx

--- app/giangvien/page.tsx
+++ app/giangvien/page.tsx
@@ -1,16 +1,27 @@
 import React, { ReactNode } from 'react';
 import Header from '../ components/Header';
-interface LayoutProps {
-  children: ReactNode;
+import { getCurrentUser } from '@/actions/getCurrentUser';
+import NullData from '../ components/NullData';
+import Container from '../ components/Container';
+import FormWrap from '../ components/FormWrap';
+import GiangVienLogin from '../Giangvienlogin/LoginForm';
+import GiangvienClient from './giangvien';
+
+const giangvien = async() => {
+  const currentUser = await getCurrentUser()
+  if(!currentUser || currentUser.role !=="GIANGVIEN"){
+
+      return <NullData title="Không thể truy cập"/>
+
+      return <NullData title="Rất tiếc! Quyền truy cập bị từ chối "/>
+
+  }
+  return (  <div className="p-8 ">
+  <Container>
+      <FormWrap>
+          <GiangvienClient children={undefined}/>
+      </FormWrap>
+      </Container></div>);
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return (
-    <div>
-      <Header />
-      <main>{children}</main>
-    </div>
-  );
-};
-
-export default Layout;
+export default giangvien;
