35b8859b1c9b9cdd87489a6292de82fda73598b1
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2024-06-13 19:23:33 +0700

    Update route.ts

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,17 +1,28 @@
-import bcrypt from "bcrypt";
-import prisma from '@/libs/prismadb';
-import { NextResponse } from "next/server";
-
-export async function POST(request: Request) {
+import bcrypt from "bcrypt"
+import prisma from '@/libs/prismadb'
+import { NextResponse } from "next/server"
+export async function POST(request:Request) {
     const body = await request.json();
     const { name, email, password, role } = body;
 
+    // Validate input
+    if (!name || !email || !password || !role) {
+        return NextResponse.json({ error: 'Missing fields' }, { status: 400 });
+    }
+
+    // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user in the database
     const user = await prisma.user.create({
         data: {
-            name, email, hashedPassword, role,
+            name,
+            email,
+            hashedPassword,
+            role,
         },
     });
 
+    // Return the created user as the response
     return NextResponse.json(user);
 }
