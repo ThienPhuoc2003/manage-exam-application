f42fcbcd51b3a55e15812970977108b683bb1530
ThienPhuoc2003 <128397202+ThienPhuoc2003@users.noreply.github.com>
2024-07-01 10:59:37 +0700

    Create page.tsx

---
+++ app/giangvien/themdethi/page.tsx
@@ -0,0 +1,12 @@
+import AddExamForm from "./AddExamForm";
+
+const Page = () => {
+    return (
+        <div className="max-w-3xl mx-auto">
+            <h1 className="text-2xl font-semibold mb-4">Thêm đề thi mới</h1>
+            <AddExamForm />
+        </div>
+    );
+};
+
+export default Page;
